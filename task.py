ab4aed7a34fd13fd0ea7e6f7acffef4763d76bc0
Giuseppe Romagnuolo <Giuseppe.Romagnuolo@allenovery.com>
2019-04-07 12:20:58 +0100

    Refactor: Added new reward function

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 from physics_sim import PhysicsSim
 
 class Task():
@@ -17,8 +18,10 @@ class Task():
         # Simulation
         self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
         self.action_repeat = 3
-
-        self.state_size = self.action_repeat * 6
+        self.position_size = 3
+        self.euler_angle_size = 3
+        # each action is repeater 3 times, the state or pose is composed of [x, y, z, alpha, beta, gamma]
+        self.state_size = self.action_repeat * (self.position_size + self.euler_angle_size)
         self.action_low = 0
         self.action_high = 900
         self.action_size = 4
@@ -28,19 +31,53 @@ class Task():
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        # reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        reward = 10. - 0.3 * np.sqrt(((self.sim.pose[:3] - self.target_pos)**2).sum())
+        # return reward
+
         return reward
+    
+    def get_directional_reward(self, previous_pose):
+        """Uses current pose of sim to return reward."""
+        distance_tnow = np.sqrt(((self.sim.pose[:3] - self.target_pos)**2).sum())
+        distance_tbefore = np.sqrt(((previous_pose[:3] - self.target_pos)**2).sum())
+
+        # boost reward when near goal
+        step_point = 10 if distance_tbefore > 1 else 100
+
+        # advancement towards target relative to previous state (distance) normalised by the initial distance
+        # if quad moves towards the target this value is positive, otherwise it is negative
+        proportion_advancement = ((distance_tbefore - distance_tnow) / (1 + distance_tbefore))
+
+        reward = step_point * proportion_advancement
+
+        return reward
+    
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
-        reward = 0
+        rewards = []
         pose_all = []
+
+        pose_all.append(self.sim.pose)
+
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
+
+            rewards.append(self.get_directional_reward(pose_all[-1]))
+            # rewards.append(self.get_reward())
             pose_all.append(self.sim.pose)
-        next_state = np.concatenate(pose_all)
-        return next_state, reward, done
+
+            # if(done):
+            #     break
+
+        next_state = np.concatenate(pose_all[1:])
+
+        relative_reward = np.sum(rewards)
+
+        # print(relative_reward)
+
+        return next_state, relative_reward, done
 
     def reset(self):
         """Reset the sim to start a new episode."""
